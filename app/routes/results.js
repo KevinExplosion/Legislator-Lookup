32365328fdd51a5466b1b6820e0d0b89ac4d004a
Dustin Davis and kevin mattison <mattisonii@yahoo.com>
2016-04-05 11:25:40 -0700

    Add new objects to API call.

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(zipInput) {
-    debugger;
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=3b10e510ff1b4db3a95ad4cfa6d67214&zip=97201';
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=3b10e510ff1b4db3a95ad4cfa6d67214&zip=' + zipInput.zippy;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
